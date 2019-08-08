e6b7a0e702190ff35b00481f076bf6145e66a29b
github username <github email address>
2019-08-08 17:15:54 -0400

    added reducers and actions folders

--- tenant-receipt-generator-client/src/index.js
+++ tenant-receipt-generator-client/src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
+let reducer = () => {
+  return "hi"
+}
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
-  <App />,document.getElementById('root'));
+  <Provider store={store}>
+  <App />
+  </Provider>
+  ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
