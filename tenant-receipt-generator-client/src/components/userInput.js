d52206df50f405711f756e1fcad107b96d832ee1
github username <github email address>
2019-08-08 22:13:25 -0400

    user form handleOnChange implemented and working

--- tenant-receipt-generator-client/src/components/userInput.js
+++ tenant-receipt-generator-client/src/components/userInput.js
@@ -4,36 +4,42 @@ import React, { Component } from 'react';
 // this is supposed to be a functional component in the component folder.
 class User extends Component {
 
-  constructor() {
-    super()
-      //the user state will go here.
-      // User.create(name: "Jane Boles", street: "65 Duer St.", city: "Dunnellen", state: "NJ", zip: 07060)
-  }
+state = {
+  name: '',
+  street: '',
+  city: '',
+  state: '',
+  zip: null
+}
 
+handleOnChange = event => {
+  this.setState({
+  [event.target.name]: event.target.value})
+}
 
-// this will have handleOnChange and handleonSubmit methods
-// will inherent action props from Home
-// will pass down the action to the connect method
+handleonSubmit = event => {
+  event.preventDefault()
+}
+
 
   render() {
     return(
 
       <div>
-
-      <h2>Landlord information</h2>
-      <form>
-        <label>Name: </label>
-        <input type="text" name="name" /><br />
-        <label>Street Address: </label>
-        <input type="text" name="address 1" /><br />
-        <label>City: </label>
-        <input type="text" name="city" /><br />
-        <label>State: </label>
-        <input type="text" name="state" /><br />
-        <label>Zip-code: </label>
-        <input type="text" name="zip-code" /><br />
-        <input type="submit" />
-      </form>
+        <h2>Landlord information</h2>
+        <form onSubmit={this.handleonSubmit}>
+          <label>Name: </label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleOnChange} /><br />
+          <label>Street Address: </label>
+            <input type="text" name="street" value={this.state.street} onChange={this.handleOnChange}  /><br />
+          <label>City: </label>
+            <input type="text" name="city"value={this.state.city} onChange={this.handleOnChange} /><br />
+          <label>State: </label>
+            <input type="text" name="state" value={this.state.state} onChange={this.handleOnChange} /><br />
+          <label>Zip-code: </label>
+            <input type="text" name="zip" value={this.state.zip} onChange={this.handleOnChange} /><br />
+            <input type="submit" />
+        </form>
       </div>
 
 
