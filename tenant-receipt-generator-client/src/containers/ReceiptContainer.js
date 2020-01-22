35bcd51f20db821334eb05f778627a67b3c22562
Josely castro <joselycas@gmail.com>
2020-01-22 11:21:03 -0500

    remove spaces

--- tenant-receipt-generator-client/src/containers/ReceiptContainer.js
+++ tenant-receipt-generator-client/src/containers/ReceiptContainer.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import ReceiptInput from '../components/ReceiptInput'
 import Receipts from '../components/Receipts'
-import {Route} from 'react-router'
-
 
 class ReceiptContainer extends Component {
 
   render() {
     return(
-
       <div>
         <ReceiptInput user={this.props.user && this.props.user.id} /><br/><br />
         <Receipts receipts={this.props.user && this.props.user.receipts} />
       </div>
-
-
     )
   }
 }
-
-
 
 export default ReceiptContainer;
