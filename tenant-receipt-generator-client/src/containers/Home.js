40416c120626e7a85e5b48f2a21574d087500c1f
Josely castro <joselycas@gmail.com>
2020-01-22 11:10:04 -0500

    format

--- tenant-receipt-generator-client/src/containers/Home.js
+++ tenant-receipt-generator-client/src/containers/Home.js
@@ -7,32 +7,15 @@ import {Button} from 'react-bootstrap'
 
 class Home extends Component {
 
-
-
   render() {
     return(
-
-    <div>
-
-
-      <Button variant="info" href="/users/new">Step 1. Landlord Information</Button>
-      <Button variant="info" href="/receipts/new">Step 2. Tenant Information, Review, and Submit!</Button><br /><br/>
-      <UserContainer />
-
-
-    </div>
-
-
+      <div>
+        <Button variant="info" href="/users/new">Step 1. Landlord Information</Button>
+        <Button variant="info" href="/receipts/new">Step 2. Tenant Information, Review, and Submit!</Button><br /><br/>
+        <UserContainer />
+      </div>
     )
   }
 }
 
 export default Home;
-
-// <ul>
-//   <li><Link to="/">Home</Link></li>
-//   <li><Link to="/users/:id/receipts/new">New Receipt</Link></li>
-//   <li><Link to="/users/:id/receipts">Receipt History</Link></li>
-//   <li><Link to="/users">Users</Link></li>
-//   <li><Link to='/users/:id'>User</Link></li>
-// </ul>
