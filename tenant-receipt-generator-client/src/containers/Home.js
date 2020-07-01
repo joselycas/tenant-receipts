8cd6be9131ecacc3535e6a7bf501a06b5b77fcda
Josely castro <joselycas@gmail.com>
2020-07-01 15:03:15 -0400

    add h1 text to home page

--- tenant-receipt-generator-client/src/containers/Home.js
+++ tenant-receipt-generator-client/src/containers/Home.js
@@ -12,6 +12,7 @@ class Home extends Component {
     return(
       <div>
         <NavBar /> <br />
+        <h1>Simply generate your receipts by filling out these forms</h1><br />
         <Button variant="info" href="/users/new">Step 1. Landlord Information</Button>
         <Button variant="info" href="/receipts/new">Step 2. Tenant Information, Review, and Submit!</Button><br /><br/>
         <UserContainer />
