dc1f88f8d8427e9ae7e29dc21f719493a49ab231
Josely castro <joselycas@gmail.com>
2020-07-01 14:47:41 -0400

    move navbar to home component

--- tenant-receipt-generator-client/src/App.js
+++ tenant-receipt-generator-client/src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './containers/Home.js';
-import NavBar from './components/NavBar'
-
-
-
 
 class App extends Component {
-
-
   render(){
   return (
     <div className="App">
     <header className="Header">Say goodbye to paper receipts.</header>
-  
-
-      <NavBar /> <br />
+   
       <Home />
-
-
-
     </div>
   );
   }
